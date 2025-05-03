aab4e090b039229f16f0f2308bc4d02705c61ba8
Ivgeni Darinski <3kbjutpr@anonaddy.me>
2025-05-02 23:51:06 -0400

    Configure Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+// Tailwind CSS configuration
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica",
+          "Arial",
+          "sans-serif",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
